a979c705a755450dae6369308b38e76433516e7f
nguyencaohuan1997@gmail.com <nguyencaohuan1997@gmail.com>
2021-10-20 11:14:20 +0700

    make it better

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,32 @@
+# Create flask app
 from flask import (
    Flask 
 )
 from flask_sqlalchemy import SQLAlchemy
 
+# Mysql primary 
+import mysql.connector
+
 application = Flask(
     __name__,
     template_folder="../layer/A_PUI_layer"
 )
-application.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://b5ccecc68435b4:715fc6a8@us-cdbr-east-04.cleardb.com/heroku_ce6f8f81c233b36"
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-database = SQLAlchemy(application)
+
+# # ORM with sqlalchemy
+# application.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://b5ccecc68435b4:715fc6a8@us-cdbr-east-04.cleardb.com/heroku_ce6f8f81c233b36"
+# application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# database = SQLAlchemy(application)
+
+# application.config['MYSQL_HOST'] = 'us-cdbr-east-04.cleardb.com'
+# application.config['MYSQL_USER'] = 'b5ccecc68435b4'
+# application.config['MYSQL_PASSWORD'] = '715fc6a8'
+# application.config['MYSQL_DB'] = 'heroku_ce6f8f81c233b36'
+
+# Primary way to connect with mysql
+# Config
+database = mysql.connector.connect(
+    host='us-cdbr-east-04.cleardb.com',
+    user="b5ccecc68435b4",
+    password="715fc6a8",
+    database="heroku_ce6f8f81c233b36"
+)
